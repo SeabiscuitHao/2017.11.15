d486ce6fb47ee96191c1bebde834744896d4b953
SeabiscuitHao <1017903523@qq.com>
2017-11-15 21:38:15 +0800

    2017.11.15

---
+++ think/application/admin/model/Admin.php
@@ -0,0 +1,18 @@
+<?php
+namespace app\admin\model;
+use think\Model;
+class Admin extends Model
+{
+  public function add_admin($data)
+  {
+    if (empty($data) || !is_array($data)) {
+      return false;
+    }else{
+      if ($this -> save($data)) {
+        return true;
+      }else {
+        return false;
+      }
+    }
+  }
+}
